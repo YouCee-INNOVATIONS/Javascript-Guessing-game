139194081172a18a37fa032fb4d45ee2ee60ec09
YouCee <64170266+YouCee-INNOVATIONS@users.noreply.github.com>
2023-01-13 22:21:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function guessNumber() {
+  let randomNum = Math.floor(Math.random() * 11);
+
+  //output to console for debugging
+  console.log(randomNum)
+
+  //declare a variable for the guess
+  let guess;
+  guess = prompt("please enter a number between 0 and 2")
+}
